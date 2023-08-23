423864ecf491014ee9dbabf357ad34e6f6da99e5
Dan <93287375+danik9@users.noreply.github.com>
2023-08-23 18:07:58 -0400

    Add files via upload

--- python scripts/relay8.py
+++ python scripts/relay8.py
@@ -8,6 +8,7 @@ relay = discord.Client(intents=intents)
 
 relay_token = "BOT_TOKEN_HERE"
 channel_dupe_id = 0123456789011121314  # Replace with your duplicate channel ID
+allowed_user_id = 0123456789011121314  # Replace with the allowed user's ID
 
 @relay.event
 async def on_ready():
@@ -19,16 +20,18 @@ async def on_message(message):
     if message.author.bot:
         return  # Ignore messages from other bots
 
-    relay_channel = relay.get_channel(channel_dupe_id)
-    if relay_channel:
-        formatted_name = f'**{message.author.name}**'
-        content = message.content
+    if isinstance(message.channel, discord.DMChannel) and message.author.id == allowed_user_id:
+        relay_channel = relay.get_channel(channel_dupe_id)
+        if relay_channel:
+            content = message.content
 
-        print(f"Received message: {content} from {formatted_name} in channel {message.channel.id}")
-        print(f'Relaying message: {content}')
-        await relay_channel.send(content)
+            print(f"Received message: {content} from {message.author.name} in channel {message.channel.id}")
+            print(f'Relaying message: {content}')
+            await relay_channel.send(content)
+        else:
+            print("Relay channel not found")
     else:
-        print("Relay channel not found")
+        print("Ignoring unauthorized or non-DM message")
 
 print("Starting relay bot...")
 relay.run(relay_token)
